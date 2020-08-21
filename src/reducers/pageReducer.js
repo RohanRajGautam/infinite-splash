a1b148e6b118c92cdd70856ababc701f6469d84d
rohan <rohanrajgautam@gmail.com>
2020-08-21 10:58:03 +0545

    :wink: added gh-pages

--- src/reducers/pageReducer.js
+++ src/reducers/pageReducer.js
@@ -1,6 +1,6 @@
 import { IMAGES } from '../constants';
 
-const pageReducer = (state = 1, action) => {
+const pageReducer = (state = 2, action) => {
   switch (action.type) {
     case IMAGES.LOAD_SUCCESS:
       return state + 1;
