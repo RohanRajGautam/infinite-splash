02f4f5b4d42f090fd9c7832b445c97c4b73cdea1
rohan <rohanrajgautam@gmail.com>
2020-08-21 11:02:44 +0545

    Updates

---
+++ precache-manifest.8e118b1d1a671e49f56cee1b19198953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c3dad5eb0c813622eea6a22d49ac46",
+    "url": "/infinite-splash/index.html"
+  },
+  {
+    "revision": "3e27a0e2db3d6a3067de",
+    "url": "/infinite-splash/static/css/main.7a116d88.chunk.css"
+  },
+  {
+    "revision": "20d2b49da25bcd5cd96a",
+    "url": "/infinite-splash/static/js/2.20b16e01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/infinite-splash/static/js/2.20b16e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e27a0e2db3d6a3067de",
+    "url": "/infinite-splash/static/js/main.b3ab9692.chunk.js"
+  },
+  {
+    "revision": "66a4599e6d8b5c0187e7",
+    "url": "/infinite-splash/static/js/runtime-main.7a60b135.js"
+  }
+]);
